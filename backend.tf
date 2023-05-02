fc680a9ecc1afc22f1b6979c437dd0c23e9bcb69
Ubuntu <ubuntu@ip-172-31-93-65.ec2.internal>
2023-05-02 10:58:54 +0000

    adding

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
- terraform {
-   backend "s3" {
-     bucket = "sowjiexmpleterraform-project-name-bootstrap-terraform-state"
-     key    = "default-infrastructure"
-     region = "ap-south-1"
-   }
-}
+# terraform {
+ #  backend "s3" {
+  #   bucket = "sowjiexmpleterraform-project-name-bootstrap-terraform-state"
+   #  key    = "default-infrastructure"
+    # region = "ap-south-1"
+   #}
+#}
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "aowjiexampleterraform-project-name-bootstrap-terraform-state"
